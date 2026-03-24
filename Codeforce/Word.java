004cfed328704c2c013e4d2cc896762e03fd31cc
sandyy404 <sp8251960@gmail.com>
2026-03-24 19:20:11 +0530

    commit

---
+++ Codeforce/Word.java
@@ -0,0 +1,29 @@
+package Codeforce;
+
+
+import java.util.Scanner;
+
+public class Word {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int upcase = 0;
+        int lcase = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                upcase++;
+            } else {
+                lcase++;
+            }
+        }
+        if (upcase <= lcase) {
+            s = s.toLowerCase();
+        }
+        if (upcase > lcase) {
+            s = s.toUpperCase();
+        }
+        System.out.println(s);
+    }
+}
