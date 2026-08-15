fae3097f488f45462ab2fe31e9c00c3cb5e0afbc
sandyy404 <sp8251960@gmail.com>
2026-08-15 11:17:58 +0530

    one more down in cses

--- CSES/Repetitions.java
+++ CSES/Repetitions.java
@@ -1,5 +1,36 @@
 package CSES;
 
+import java.util.Scanner;
+
 public class Repetitions {
-   
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int len = 0;
+        int maxlen = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) != s.charAt(i + 1)) {
+                len++;
+                if (len > maxlen) {
+                    maxlen = len;
+                }
+                len = 0;
+            } else {
+                len++;
+                // if (len > maxlen) {
+                //     maxlen = len;
+                // }
+            }
+        }
+        if (s.length() > 1) {
+            if (s.charAt(s.length() - 2) == s.charAt(s.length() - 1)) {
+                len++;
+            }
+        } else {
+            len++;
+        }
+        System.out.println(Math.max(len, maxlen));
+
+    }
 }
